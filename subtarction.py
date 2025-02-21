6d5c777e0f1bf208920a826a17628e96fb1beebc
aravinda4242 <ramadasaravinda2424@gmail.com>
2025-02-21 12:39:57 +0530

    Create subtarction.py

---
+++ subtarction.py
@@ -0,0 +1,11 @@
+# Subtraction of two numbers in Python
+
+# Input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Subtracting the numbers
+result = num1 - num2
+
+# Displaying the result
+print(f"The result of subtraction is: {result}")
